11852715826e494677a28c031bf3d0f815381d3d
minoru mikoda <minoru.mikoda@gmail.com>
2020-12-15 15:02:05 +0000

    プロフィール画像登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,:password_confirmation,
+                                 :photo, :photo_cache)
   end
 end
